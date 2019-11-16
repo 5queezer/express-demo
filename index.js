3fe939b64d61cd91ca205b951170eb25290d2c05
Christian Pojoni <dev@pojoni.at>
2019-11-16 19:50:22 +0100

    middleware

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');  
 const logger = require('./logger');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'));
+app.use(express.static('public'));
+app.use(helmet());
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  console.log('Morgan enabled...');
+}
 
 app.use(logger);
 
-app.use(function(req, res, next) {
-  console.log('Authenticating...');
-  next();
-});
 
 const courses = [
   { id: 1, name: 'course1' },
