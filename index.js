f8c9e9e3c74689c9377589d521e4f64ab469a6f3
Christian Pojoni <dev@pojoni.at>
2019-11-16 20:21:03 +0100

    debug scopes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const config = require('config');
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');  
@@ -18,8 +20,11 @@ console.log('Mail Password: ' + config.get('mail.password'));
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
-  console.log('Morgan enabled...');
+  startupDebugger('Morgan enabled...');
 }
+
+// Db work...
+dbDebugger('Connected to the database...');
 
 app.use(logger);
 
