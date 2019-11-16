a6e683b862cff5be9ebd201caf0928a770c27c87
Christian Pojoni <dev@pojoni.at>
2019-11-16 19:22:25 +0100

    middleware

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  console.log('Loggin\'...')
+});
+
+app.use(function(req, res, next) {
+  console.log('Authenticating...')
+});
+
 const courses = [
   { id: 1, name: 'course1' },
   { id: 2, name: 'course2' },
